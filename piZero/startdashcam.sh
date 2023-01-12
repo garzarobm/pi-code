4aed36a05bc13da218f0d8883cffa6820905160b
garzarobm <garzarobm@gmail.com>
2023-01-12 16:56:33 -0600

    added starting dashcam file

--- piZero/startdashcam.sh
+++ piZero/startdashcam.sh
@@ -3,5 +3,8 @@
 # remove files older than 3 days
 find /home/pi/DashVideos -type f -iname '*.flv' -mtime +3 -exec rm {} \;
 
-# start dashcam service
-sudo systemctl start dashcam
+# start dashcam service 
+#sudo systemctl start dashcam
+
+#start dashcam file
+/bin/bash /home/pi/Desktop/pi-code/piZero/dashcam.sh
