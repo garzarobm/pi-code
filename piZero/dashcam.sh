86624106189a32e1b2629842786a7cf2298d13ce
garzarobm <garzarobm@gmail.com>
2023-01-12 17:55:31 -0600

    changed dashcam

--- piZero/dashcam.sh
+++ piZero/dashcam.sh
@@ -4,25 +4,26 @@
 when=$(date)
 
 # make sure dashcam.log is present touch dashcam.log
-if [ -e dashcam.log ]
+echo "looking for log file"
+if [ -e /home/pi/Desktop/Videos/dashcam.log ]
 then
     echo "found previous dashcam.log"
 else
     echo "no dashcam log file \n creating one now"
-    touch  dashcam.log
+    touch  /home/pi/Desktop/Videos/dashcam.log
 fi
 # mv the current video to the same name but with the date added  
-if [ -e dashcam.flv ]
+if [ -e /home/pi/Desktop/Videos/dashcam.flv ]
 then
     echo "found previous recording"
-    mv dashcam.flv $(date+%F-%H:%M).dashcam.flv
+    mv dashcam.flv /home/pi/Desktop/Videos/$(date+%F-%H:%M).dashcam.flv
 
 else
     echo "no previous video found \n starting dashcam"
 fi
 
 # log to dashcam.log every time the service is started 
-echo "Started video at: $when" >> dashcam.log
+echo "Started video at: $when" >> /home/pi/Desktop/Videos/dashcam.log
 
 # record at 1024x760 with a Desktop preview window of 640x480, pipe to ffmpeg and output dashcam.flv 
-raspivid -t 0 -w 1024 -h 760 -fps 25 -b 5000000 -p 0,0.640,480 -vf -o - | ffmpeg -i - -vcodec copy -an -f flv -r 25 -pix_fmt yuv420p dashcam.flv -y
+raspivid -t 0 -w 1024 -h 760 -fps 25 -b 5000000 -p 0,0.640,480 -vf -o - | ffmpeg -i - -vcodec copy -an -f flv -r 25 -pix_fmt yuv420p /home/pi/Desktop/Videos/dashcam.flv -y
