409d890ca5845f81d6f72d3a22653d1b358b4d97
garzarobm <garzarobm@gmail.com>
2023-01-12 21:20:53 -0600

    working upload to nextcloud

--- piZero/dashcam.sh
+++ piZero/dashcam.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
+export folder_path="/home/pi/Desktop/Videos/recordings"
+export log_file_path="/home/pi/Desktop/Videos/recordings/dashcam.log"
+export video_file_path="/home/pi/Desktop/Videos/recordings"
+
 
 # set variable form the date 
 when=$(date)
 
 # make sure dashcam.log is present touch dashcam.log
 echo "looking for log file"
-if [ -e /home/pi/Desktop/Videos/dashcam.log ]
+if [ -e $log_file_path ]
 then
     echo "found previous dashcam.log"
 else
     echo "no dashcam log file \n creating one now"
-    touch  /home/pi/Desktop/Videos/dashcam.log
+    touch  $log_file_path
 fi
 # mv the current video to the same name but with the date added  
-if [ -e /home/pi/Desktop/Videos/dashcam.flv ]
+if [ -e $video_file_path ]
 then
     echo "found previous recording"
-    mv dashcam.flv /home/pi/Desktop/Videos/$(date+%F-%H:%M).dashcam.flv
+    mv dashcam.flv $folder_path/$(date+%F-%H:%M).dashcam.flv
 
 else
     echo "no previous video found \n starting dashcam"
 fi
 
 # log to dashcam.log every time the service is started 
-echo "Started video at: $when" >> /home/pi/Desktop/Videos/dashcam.log
+echo "Started video at: $when" >> $video_file_path
 
 # record at 1024x760 with a Desktop preview window of 640x480, pipe to ffmpeg and output dashcam.flv 
-raspivid -t 0 -w 1024 -h 760 -fps 25 -b 5000000 -p 0,0.640,480 -vf -o - | ffmpeg -i - -vcodec copy -an -f flv -r 25 -pix_fmt yuv420p /home/pi/Desktop/Videos/dashcam.flv -y
+raspivid -t 0 -w 1024 -h 760 -fps 25 -b 5000000 -p 0,0.640,480 -vf -o - | ffmpeg -i - -vcodec copy -an -f flv -r 25 -pix_fmt yuv420p $video_file_path -y
--- piZero/python/test/file_upload_to_server.py
+++ piZero/python/test/file_upload_to_server.py
@@ -1,5 +1,8 @@
 import nextcloud_client
 from myconfig import *
+
+#local variables
+file_name="test.txt"
 nc = nextcloud_client.Client(driveUrl)
 
 #logging in 
@@ -7,8 +10,8 @@ print("logging in...\n")
 nc.login(piDash0_username, piDash0_pass)
 
 
-print("sending test file now to: " + piDash0_USBPath)
-nc.put_file(piDash0_USBPath + '/test.txt', 'test.txt')
+print("sending test file " + file_name + " now to: " + piDash0_USBPath)
+nc.put_file(piDash0_USBPath + '/' + file_name, file_name)
 
 #link_info = nc.share_file_with_link(piDash0_localPath + '/test.txt')
 
