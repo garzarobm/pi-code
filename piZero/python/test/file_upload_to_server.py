68257b1c1ec2fbcd58f14444b0cf9bff397c455e
garzarobm <garzarobm@gmail.com>
2023-01-12 19:55:20 -0600

    edited client again

--- piZero/python/test/file_upload_to_server.py
+++ piZero/python/test/file_upload_to_server.py
@@ -2,10 +2,12 @@ import nextcloud_client
 from myconfig import *
 nc = nextcloud_client.Client(driveUrl)
 
+#logging in 
+print("logging in...\n")
 nc.login(piDash0_username, piDash0_pass)
 
 
-
+print("sending test file now to: " + piDash0_USBPath)
 nc.put_file(piDash0_USBPath + '/test.txt', 'test.txt')
 
 #link_info = nc.share_file_with_link(piDash0_localPath + '/test.txt')
